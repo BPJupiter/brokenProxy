3d1f3bb29eafd7e103249c4c2c86f1f476b50c2f
BPJupiter <alex.francisbt@gmail.com>
2025-12-19 20:18:28 +1300

    Cleaned up directory structure

--- proto-c/proxy.h
+++
@@ -1,7 +0,0 @@
-#ifndef PROXY_H
-#define PROXY_H
-
-void proxy_init(unsigned short (*hostname_resolver)(unsigned char* hostname, unsigned char*** answer_index), double (*tracert)(const char* ip, char* out_buf, size_t out_size), double rtt_cutoff);
-int proxy_start(int port);
-
-#endif
