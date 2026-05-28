d3d512412af69446f9f87bd90aa5ef67118622d2
BPJupiter <alex.francisbt@gmail.com>
2026-05-28 17:56:57 +1200

    Brainstorming in proxy.c

--- src/proxy/proxy.c
+++ src/proxy/proxy.c
@@ -68,6 +68,76 @@ enum socks_status {
 	UNSUPPORTED_ADDRESS = 0x08
 };
 
+/* 
+* The below is a brainstorm for how I want to restructure my measurement data structure to:
+* 1. Increase ease of use
+* 2. Reduce data duplication
+* 3. Easily map to SQL entries
+*/
+
+#define MAX_MEASUREMENTS (1 << 16)
+typedef uint midx;
+
+typedef enum {
+	MF_NIL = (1 << 0),
+	MF_PING = (1 << 1),
+	MF_TRACEROUTE = (1 << 2),
+	MF_MAX_FLAGS = (1 << 31)
+} MeasurementFlags;
+
+typedef enum {
+	RB_NIL = 0,
+	RB_DNS,
+	RB_HOP,
+	RB_HOST,
+} ReachabilityBlame;
+
+typedef enum {
+	RT_NIL = 0,
+	RT_CABLE_BROKEN,
+	RT_EXCEEDS_MAX_LATENCY,
+	RT_REACHABLE,
+} ReachabilityType;
+
+typedef struct {
+	union {
+		uint32 v4;
+		uint8 v6[16];
+	} address; /* objective*/
+	char domain[256]; /* objective */
+	boolean is_ipv6; /* objective */
+	float rtt; /* subjective */
+	time_t timestamp; /* objective */
+
+	midx hops[32]; /* objective */
+	uint hop_count; /* objective */
+
+	MeasurementFlags measurements_performed; /* objective */
+	struct {
+		ReachabilityType type;
+		struct {
+			ReachabilityBlame reason;
+			midx host;
+		} blame;
+	} reachability;
+} IpMeasurement;
+
+/* SQL QUESTION:
+ * Do I even bother with this? Given how fast an SQLite in-memory database can be.
+ * Turns out there is the SQLite Online Backup API that allows syncing between in-memory and on-disk databases.
+ * Multi-producer, single consumer queue. Dozens of reader threads, one writer thread, reader threads queue data for the writer thread to batch.
+ * SQLite WAL mode?
+ * Would end up puttings this in data/data.h
+*/
+typedef struct {
+	IpMeasurement	ip_measurements[MAX_MEASUREMENTS];
+	boolean			used[MAX_MEASUREMENTS]; /* may want to move away from C89 to allow for 1 bit array ? Or just use an int array with bitmasking */
+	midx			next_empty_slot; /* 0 slot is nil */
+	midx (*add)(IpMeasurement measurement);
+	void (*rem)(midx idx);
+	IpMeasurement *(*get)(midx idx);
+} Measurements;
+
 static int proxy_printf(const char *format, ...);
 #define printf proxy_printf
 
