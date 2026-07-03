f70e3bee226cbf157ea95dc3c095d46e768dbe30
BPJupiter <alex.francisbt@gmail.com>
2026-07-03 23:20:32 +1200

    Updated build scripts

--- build_linux.sh
+++ build_linux.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
-
 OUT_DIR="Debug"
-OUT_EXE="example_sdl3_sdlrenderer3"
+OUT_EXE="BrokenProxy"
 INCLUDES="-I. \
           -Ithird_party/imgui/include \
           -Ithird_party/portable_file_dialogs/include \
           -Ithird_party/nlohmann/include \
           -Ithird_party/nanosvg/include \
           -Ithird_party/mapbox/include \
+          -Ithird_party/cjson/include \
+          -Isrc/shared \
+          -Isrc/proxy \
           $(pkg-config --cflags sdl3)"
-SOURCES="main.cpp third_party/imgui/src/imgui*.cpp"
+SOURCES="third_party/imgui/src/imgui*.cpp \
+         third_party/cjson/src/cJSON.c \
+         src/shared/Clay/*.c \
+         src/shared/Europa/*.c \
+         src/shared/Styx/*.c \
+         src/shared/Talos/*.c \
+         src/proxy/bproxy.c \
+         src/proxy/dns_resolution.c \
+         src/proxy/network_tools.c \
+         src/main.cpp"
 LIBS="$(pkg-config --libs sdl3)"
 mkdir -p ${OUT_DIR}
 echo "Compiling..."
